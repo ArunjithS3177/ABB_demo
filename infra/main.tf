520b643f4ad4279c12127cca6e90550968148b59
Raghunandan <Raghunandan.Pattar@dominos.co.uk>
2025-02-20 16:40:13 +0530

    made some enhancements for the pipeline and increaed the security for the aks cluster

--- infra/main.tf
+++ infra/main.tf
@@ -6,18 +6,25 @@ resource "azurerm_log_analytics_workspace" "aks_logs" {
   retention_in_days   = 30
 }
 
-
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  dns_prefix          = "myaks"
+  name                              = var.aks_cluster_name
+  location                          = var.location
+  resource_group_name               = var.resource_group_name
+  dns_prefix                        = "myaks"
+  role_based_access_control_enabled = true
+
 
   default_node_pool {
     name       = "agentpool"
     node_count = 2
     vm_size    = "Standard_DS2_v2"
   }
+
+  network_profile {
+    network_plugin = "azure"  # "azure" for CNI, "kubenet" for basic networking
+    network_policy = "calico" # Adds Network Policy support (Calico for better security)
+  }
+
 
   identity {
     type = "SystemAssigned"
@@ -27,6 +34,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   oms_agent {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.aks_logs.id
   }
+
+
 
   tags = {
     environment = "Prod"
