b73939ec066d40d07aed8ca163b88336e1cc679f
AzureDemoABB <raghuabbdemo@gmail.com>
2025-02-20 19:40:50 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,18 +1,8 @@
-resource "azurerm_log_analytics_workspace" "aks_logs" {
-  name                = "aks-monitor-logs"
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                              = var.aks_cluster_name
-  location                          = var.location
-  resource_group_name               = var.resource_group_name
-  dns_prefix                        = "myaks"
-  role_based_access_control_enabled = true
-
+  dns_prefix          = "abbclusterdemo"
 
   default_node_pool {
     name       = "agentpool"
@@ -20,22 +10,22 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = "Standard_DS2_v2"
   }
 
-  network_profile {
-    network_plugin = "azure"  # "azure" for CNI, "kubenet" for basic networking
-    network_policy = "calico" # Adds Network Policy support (Calico for better security)
-  }
-
-
   identity {
     type = "SystemAssigned"
   }
 
-  # Enable Azure Monitor for containers
-  oms_agent {
-    log_analytics_workspace_id = azurerm_log_analytics_workspace.aks_logs.id
+  monitor_metrics {
+    enabled = true  # Enables Azure Monitor Agent (AMA)
   }
 
-
+  azure_monitor_profile {
+    metrics {
+      annotations_allowed = ["app.kubernetes.io/name"]
+      collection_interval = 60
+      enabled             = true
+      extra_labels_allowed = ["environment"]
+    }
+  }
 
   tags = {
     environment = "Prod"
